4b0f5f926b86e5641f41b18f9bc16aeda31736ff
Dmitry Kuguchev <kuguchev.d@micro-system.ru>
2025-12-03 01:26:44 +0300

    Add entrypoint.sh script and integrate it into Dockerfile

---
+++ scripts/entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+set -e
+
+if [ ! -f ~/data/"${APP_SQLITE_DB_FILE}".db ]; then
+  cp /usr/local/bin/"${APP_SQLITE_DB_FILE}".db ~/data/"${APP_SQLITE_DB_FILE}".db
+fi
+
+cd ~/data
+
+exec "${APP_REPO}" "$@"
